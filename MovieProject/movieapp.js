c0241f3d8f73fa425028e72e9857be9279f3b45f
medinadev35 <amandamedinamiguel@gmail.com>
2020-04-10 21:28:29 -0500

    Started favorites

--- MovieProject/movieapp.js
+++ MovieProject/movieapp.js
@@ -1,24 +1,53 @@
 Vue.component('movie-detail',{
 
     props: ['movie'],
+
+
+
+    methods:{
+        addFavorite(){
+            this.favorites.add(this.book);
+        },
+    },
     template: '<div><div class="card" style="width: 18rem;">\n' +
-        '  <img class="card-img-top" v-bind:src="movie.Poster" alt="">\n' +
+        '  <img class=".card-columns" v-bind:src="movie.Poster" alt="">\n' +
         '  <div class="card-body">\n' +
         '    <h5 class="card-title">{{movie.Title}}</h5>\n' +
         '    <p class="card-text"></p>\n' +
-        '    <a href="#" class="btn btn-primary">Details</a>\n' +
+        '    <a href="#" class="btn btn-primary">Details</a><div class="favoriting">\n' +
+        '    <label\n' +
+        '    class="favorite__heart"\n' +
+        '    v-bind:class="{\'favorite__heart__selected\': value, \'is-disabled\': disabled}"\n' +
+        '    v-on:click="addFavorite">\n' +
+        '    <input\n' +
+        '    class="favorite__checkbox"\n' +
+        '    type="checkbox"\n' +
+        '    v-bind:name="name"\n' +
+        '    v-bind:value="value"\n' +
+        '    v-bind:required="required"\n' +
+        '    v-bind:disabled="disabled"\n' +
+        '    v-model="value">\n' +
+        '                ❤\n' +
+        '    </label>\n' +
+
+        '    </div>\n' +
+
         '  </div>\n' +
+
         '</div></div>'
 
-});
+
+        });
 
 new Vue({
     el: "#movieapp",
     data: {
       searchKey: '',
-      movieList: []
-    },
+      movieList: [],
+        favorites: [],
 
+    },
+    // methods
     methods: {
 
         searchMovies()
@@ -30,6 +59,76 @@ new Vue({
                     this.movieList=data;
                 })
         }
-    }
+    },
 
+
+
+    });
+
+Vue.component('favorite', {
+    template: '#template-favorite',
+    data: function() {
+        return { };
+    },
+    props: {
+        'name': {
+            type: String,
+            default: 'favorite'
+        },
+        'value': {
+            type: Boolean,
+            default: false
+        },
+        'disabled': {
+            type: Boolean,
+            default: false
+        }
+    },
+    methods: {
+        favorite: function() {
+            if (this.disabled==true) {
+                return;
+            }
+            this.value = !this.value;
+        }
+    }
+});
+
+Vue.component('favorite', {
+    template: '#template-favorite',
+    data: function() {
+        return { };
+    },
+    props: {
+        'name': {
+            type: String,
+            default: 'favorite'
+        },
+        'value': {
+            type: Boolean,
+            default: false
+        },
+        'disabled': {
+            type: Boolean,
+            default: false
+        }
+    },
+    methods: {
+        favorite: function() {
+            if (this.disabled==true) {
+                return;
+            }
+            this.value = !this.value;
+        }
+    }
+});
+
+
+var app = new Vue({
+    el: 'body'
+});
+
+
+var app = new Vue({
+    el: 'body'
 });
